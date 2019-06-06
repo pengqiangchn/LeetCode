4132e08dc282971ebcfac1a6a1377f140763e6f4
彭强 <pengqiangchn@foxmail.com>
2019-06-06 15:49:20 +0800

    完成第38题.

---
+++ LeetCode/038-Count and Say.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution038
+    {
+        public string CountAndSay(int n)
+        {
+            if (n == 1)
+                return "1";
+
+            string say = "";
+            List<int> list = new List<int>() { 1 };
+            List<int> newList = new List<int>();
+            int temp = 0;
+            int count = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                temp = list[0];
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (temp == list[j])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        //添加并重数；
+                        newList.Add(count);
+                        newList.Add(temp);
+                        temp = list[j];
+                        count = 1;
+                    }
+
+                    //如果是最后一个数了，就添加
+                    if (j == list.Count - 1)
+                    {
+                        newList.Add(count);
+                        newList.Add(temp);
+                    }
+                }
+
+                list = newList;
+                count = 0;
+                newList = new List<int>();
+            }
+            list.ForEach(s => say += s.ToString());
+
+            return say;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/038-Count and Say Test.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution038Test
+    {
+        [Fact]
+        public void CountAndSay1()
+        {
+            //Arrange 
+            int n = 1;
+            string expected = "1";
+
+            //Act
+            Solution038 s = new Solution038();
+            string actual = s.CountAndSay(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CountAndSay4()
+        {
+            //Arrange 
+            int n = 4;
+            string expected = "1211";
+
+            //Act
+            Solution038 s = new Solution038();
+            string actual = s.CountAndSay(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void CountAndSay6()
+        {
+            //Arrange 
+            int n = 6;
+            string expected = "312211";
+
+            //Act
+            Solution038 s = new Solution038();
+            string actual = s.CountAndSay(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
