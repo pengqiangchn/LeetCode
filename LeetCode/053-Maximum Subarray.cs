1bc95932cfc1e6190ce349d61bbbf172d0e31f90
彭强 <pengqiangchn@foxmail.com>
2019-06-07 09:38:05 +0800

    053,使用暴力解法，下次学习动态规划，再解一遍

---
+++ LeetCode/053-Maximum Subarray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution053
+    {
+        //这个题目最快能用 动态规划。下次可以使用动态规划再解一遍
+
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+
+            //简单暴力的解法
+            //类似冒泡 循环两次
+            int max = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int temp = nums[i];
+                if (max < temp)
+                {
+                    max = temp;
+                }
+
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    temp += nums[j];
+                    if (max < temp)
+                    {
+                        max = temp;
+                    }
+                }
+
+            }
+
+
+            return max;
+
+        }
+    }
+}
---
+++ LeetCodeUnitTest/053-Maximum Subarray Test.cs
@@ -0,0 +1,86 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution053Test
+    {
+        [Fact]
+        public void MaxSubArray()
+        {
+            //Arrange
+            int[] nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            int expected = 6;
+
+            //Act
+            Solution053 s = new Solution053();
+            int actual = s.MaxSubArray(nums);
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MaxSubArray2()
+        {
+            //Arrange
+            int[] nums = new int[] { -1 };
+            int expected = -1;
+
+            //Act
+            Solution053 s = new Solution053();
+            int actual = s.MaxSubArray(nums);
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MaxSubArray3()
+        {
+            //Arrange
+            int[] nums = new int[] { 0, -1, 1, 0 };
+            int expected = 1;
+
+            //Act
+            Solution053 s = new Solution053();
+            int actual = s.MaxSubArray(nums);
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MaxSubArray4()
+        {
+            //Arrange
+            int[] nums = new int[] { -2, 1 };
+            int expected = 1;
+
+            //Act
+            Solution053 s = new Solution053();
+            int actual = s.MaxSubArray(nums);
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MaxSubArray5()
+        {
+            //Arrange
+            int[] nums = new int[] { -1, 0, -2 };
+            int expected = 0;
+
+            //Act
+            Solution053 s = new Solution053();
+            int actual = s.MaxSubArray(nums);
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+    }
+}
