5312172a0b886116c321a52242e8fa157eaff295
彭强 <pengqiangchn@foxmail.com>
2019-06-08 22:37:51 +0800

    058

---
+++ LeetCode/058-Length of Last Word.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution058
+    {
+        public int LengthOfLastWord(string s)
+        {
+            if (String.IsNullOrWhiteSpace(s) || s == " ")
+            {
+                return 0;
+            }
+
+            string[] array = s.Split(new char[] { ' ' });
+
+            string last = array[array.Length - 1];
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (!string.IsNullOrWhiteSpace(array[i]))
+                {
+                    return array[i].Length;
+                }
+            }
+
+            return 0;
+
+
+
+        }
+    }
+}
---
+++ LeetCodeUnitTest/058-Length of Last Word Test.cs
@@ -0,0 +1,107 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution058Test
+    {
+        [Fact]
+        public void LengthOfLastWord1()
+        {
+            //Arrange 
+            string str = "Hello World";
+            int expected = 5;
+
+            //Act
+            Solution058 s = new Solution058();
+            int actual = s.LengthOfLastWord(str);
+
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LengthOfLastWord2()
+        {
+            //Arrange 
+            string str = "He llo Worl d";
+            int expected = 1;
+
+            //Act
+            Solution058 s = new Solution058();
+            int actual = s.LengthOfLastWord(str);
+
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LengthOfLastWord3()
+        {
+            //Arrange 
+            string str = " ";
+            int expected = 0;
+
+            //Act
+            Solution058 s = new Solution058();
+            int actual = s.LengthOfLastWord(str);
+
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LengthOfLastWord4()
+        {
+            //Arrange 
+            string str = "   ";
+            int expected = 0;
+
+            //Act
+            Solution058 s = new Solution058();
+            int actual = s.LengthOfLastWord(str);
+
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LengthOfLastWord5()
+        {
+            //Arrange 
+            string str = "a ";
+            int expected = 1;
+
+            //Act
+            Solution058 s = new Solution058();
+            int actual = s.LengthOfLastWord(str);
+
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LengthOfLastWord6()
+        {
+            //Arrange 
+            string str = "b a    ";
+            int expected = 1;
+
+            //Act
+            Solution058 s = new Solution058();
+            int actual = s.LengthOfLastWord(str);
+
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
+    }
+}
