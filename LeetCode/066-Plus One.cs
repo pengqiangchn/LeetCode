daa839a4033c08470fcd57496cd16895c4d8c099
彭强 <pengqiangchn@foxmail.com>
2019-06-09 11:00:16 +0800

    066

---
+++ LeetCode/066-Plus One.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution066
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            List<int> value = new List<int>();
+
+            int temp = 0;
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                temp = digits[i] + 1;
+                value.Add(temp % 10);
+                if (temp < 10 && i != 0)
+                {
+                    for (int j = i - 1; j >= 0; j--)
+                    {
+                        value.Add(digits[j]);
+                    }
+                    break;
+                }
+                else if (temp > 9 && i == 0)
+                {
+                    value.Add(1);
+                }
+
+            }
+
+            value.Reverse();
+
+            return value.ToArray();
+        }
+    }
+}
---
+++ LeetCodeUnitTest/066-Plus One Test.cs
@@ -0,0 +1,93 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution066Test
+    {
+        [Fact]
+        public void PlusOne1()
+        {
+            //Arrange
+            int[] value = new int[] { 1, 2, 3 };
+            int[] expencted = new int[] { 1, 2, 4 };
+
+            //Act
+            Solution066 s = new Solution066();
+            int[] actual = s.PlusOne(value);
+
+            //Assert
+            Assert.Equal(expencted, actual);
+
+        }
+
+        [Fact]
+        public void PlusOne2()
+        {
+            //Arrange
+            int[] value = new int[] { 4, 3, 2, 1 };
+            int[] expencted = new int[] { 4, 3, 2, 2 };
+
+            //Act
+            Solution066 s = new Solution066();
+            int[] actual = s.PlusOne(value);
+
+            //Assert
+            Assert.Equal(expencted, actual);
+
+        }
+
+        [Fact]
+        public void PlusOne3()
+        {
+            //Arrange
+            int[] value = new int[] { 0, 2, 3, 9 };
+            int[] expencted = new int[] { 0, 2, 4, 0 };
+
+            //Act
+            Solution066 s = new Solution066();
+            int[] actual = s.PlusOne(value);
+
+            //Assert
+            Assert.Equal(expencted, actual);
+
+        }
+
+        [Fact]
+        public void PlusOne4()
+        {
+            //Arrange
+            int[] value = new int[] { 0 };
+            int[] expencted = new int[] { 1 };
+
+            //Act
+            Solution066 s = new Solution066();
+            int[] actual = s.PlusOne(value);
+
+            //Assert
+            Assert.Equal(expencted, actual);
+
+        }
+
+
+        [Fact]
+        public void PlusOne5()
+        {
+            //Arrange
+            int[] value = new int[] { 9, 9, 9, 9 };
+            int[] expencted = new int[] { 1, 0, 0, 0, 0 };
+
+            //Act
+            Solution066 s = new Solution066();
+            int[] actual = s.PlusOne(value);
+
+            //Assert
+            Assert.Equal(expencted, actual);
+
+        }
+
+    }
+}
