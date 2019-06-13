987c49e75a4fe67a8d575f94d66f4692f9abc146
彭强 <pengqiangchn@foxmail.com>
2019-06-13 16:09:30 +0800

    070，变相斐波那契问题，但是不知道为什么。。

---
+++ LeetCode/070-Climbing Stairs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution070
+    {
+        public int ClimbStairs(int n)
+        {
+            //变相的斐波那契数列
+            //int value = Fibonacci(n);
+
+            //斐波那契使用递归太慢了
+            int value = 0;
+            int f1 = 1;
+            int f2 = 2;
+            if (n == 1)
+                return f1;
+
+            if (n == 2)
+                return f2;
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                value = f1 + f2;
+                f1 = f2;
+                f2 = value;
+            }
+
+
+            return value;
+        }
+
+        ////太慢了
+        //public int Fibonacci(int n)
+        //{
+        //    if (n == 0)
+        //        return 1;
+
+        //    if (n == 1)
+        //        return 1;
+
+        //    if (n >= 2)
+        //    {
+        //        return Fibonacci(n - 1) + Fibonacci(n - 2);
+        //    }
+
+        //    return 0;
+        //}
+    }
+}
---
+++ LeetCodeUnitTest/070-Climbing Stairs Test.cs
@@ -0,0 +1,119 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution070Test
+    {
+        [Fact]
+        public void ClimbStairs1()
+        {
+            //Arrange
+            int n = 1;
+            int expected = 1;
+
+            //Act
+            Solution070 s = new Solution070();
+            int actual = s.ClimbStairs(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ClimbStairs2()
+        {
+            //Arrange
+            int n = 2;
+            int expected = 2;
+
+            //Act
+            Solution070 s = new Solution070();
+            int actual = s.ClimbStairs(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void ClimbStairs3()
+        {
+            //Arrange
+            int n = 3;
+            int expected = 3;
+
+            //Act
+            Solution070 s = new Solution070();
+            int actual = s.ClimbStairs(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void ClimbStairs4()
+        {
+            //Arrange
+            int n = 4;
+            int expected = 5;
+
+            //Act
+            Solution070 s = new Solution070();
+            int actual = s.ClimbStairs(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void ClimbStairs5()
+        {
+            //Arrange
+            int n = 5;
+            int expected = 8;
+
+            //Act
+            Solution070 s = new Solution070();
+            int actual = s.ClimbStairs(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ClimbStairs6()
+        {
+            //Arrange
+            int n = 6;
+            int expected = 13;
+
+            //Act
+            Solution070 s = new Solution070();
+            int actual = s.ClimbStairs(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ClimbStairs44()
+        {
+            //Arrange
+            int n = 44;
+            int expected = 1134903170;
+
+            //Act
+            Solution070 s = new Solution070();
+            int actual = s.ClimbStairs(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
