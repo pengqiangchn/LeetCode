e2a9731e9e593e4da78f36968afd90e353afaf0c
彭强 <pengqiangchn@foxmail.com>
2019-06-15 17:02:42 +0800

    088

---
+++ LeetCode/088-Merge Sorted Array.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution088
+    {
+        //虽然通过了，但是用了两个循环 所以速度慢
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int index1 = 0;
+            int index2 = 0;
+            int last = m;
+            int length = m + n;
+
+            while (index1 < length)
+            {
+                if (index2 >= n)
+                {
+                    break;
+                }
+
+                if (m <= 0 || (nums1[index1] > nums2[index2]))
+                {
+                    for (int i = last; i > index1; i--)
+                    {
+                        nums1[i] = nums1[i - 1];
+                    }
+
+                    nums1[index1] = nums2[index2];
+                    last++;
+                    index2++;
+                }
+                else
+                {
+                    m--;
+                }
+                index1++;
+
+            }
+
+        }
+    }
+}
---
+++ LeetCodeUnitTest/088-Merge Sorted Array Test.cs
@@ -0,0 +1,90 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution088Test
+    {
+        [Fact]
+        public void Merge1()
+        {
+            //Arrange
+            int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = new int[] { 2, 5, 6 };
+            int n = 3;
+
+            int[] expected = new int[] { 1, 2, 2, 3, 5, 6 };
+
+            //Act
+            Solution088 s = new Solution088();
+            s.Merge(nums1, m, nums2, n);
+
+            //Assert
+            Assert.Equal(expected, nums1);
+        }
+
+
+        [Fact]
+        public void Merge2()
+        {
+            //Arrange
+            int[] nums1 = new int[] { 0, 0, 0, 0, 0, 0 };
+            int m = 0;
+            int[] nums2 = new int[] { 0, 0, 0 };
+            int n = 0;
+
+            int[] expected = new int[] { 0, 0, 0, 0, 0, 0 };
+
+            //Act
+            Solution088 s = new Solution088();
+            s.Merge(nums1, m, nums2, n);
+
+            //Assert
+            Assert.Equal(expected, nums1);
+        }
+
+
+        [Fact]
+        public void Merge3()
+        {
+            //Arrange
+            int[] nums1 = new int[] { 1, 1, 1, 2, 2, 2, 0, 0, 0 };
+            int m = 6;
+            int[] nums2 = new int[] { 3, 3, 5 };
+            int n = 3;
+
+            int[] expected = new int[] { 1, 1, 1, 2, 2, 2, 3, 3, 5 };
+
+            //Act
+            Solution088 s = new Solution088();
+            s.Merge(nums1, m, nums2, n);
+
+            //Assert
+            Assert.Equal(expected, nums1);
+        }
+
+        [Fact]
+        public void Merge4()
+        {
+            //Arrange
+            int[] nums1 = new int[] { 1, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int m = 2;
+            int[] nums2 = new int[] { 3, 3, 5, 6, 8, 11, 33 };
+            int n = 7;
+
+            int[] expected = new int[] { 1, 3, 3, 5, 6, 8, 10, 11, 33, 0, 0, 0, 0 };
+
+            //Act
+            Solution088 s = new Solution088();
+            s.Merge(nums1, m, nums2, n);
+
+            //Assert
+            Assert.Equal(expected, nums1);
+        }
+
+    }
+}
