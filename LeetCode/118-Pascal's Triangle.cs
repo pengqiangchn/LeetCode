72e7de8ba01d52340eeb5c0094db61abad820193
彭强 <pengqiangchn@foxmail.com>
2019-06-16 20:52:35 +0800

    暂时跳过树,118

---
+++ LeetCode/118-Pascal's Triangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution118
+    {
+        //跟最快的类似，只是在中间的for循环中，最快的那个算法，去掉了0判断和末尾判断。
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> listArray = new List<IList<int>>();
+
+            if (numRows <= 0)
+                return listArray;
+
+            listArray.Add(new List<int>() { 1 });
+            if (numRows == 1)
+                return listArray;
+
+            listArray.Add(new List<int>() { 1, 1 });
+            if (numRows == 2)
+                return listArray;
+
+            for (int i = 3; i <= numRows; i++)
+            {
+                List<int> list = new List<int>();
+                for (int j = 0; j < i; j++)
+                {
+                    if (j == 0 || j == i - 1)
+                    {
+                        list.Add(1);
+                    }
+                    else
+                    {
+
+                        list.Add(listArray[i - 2][j] + listArray[i - 2][j - 1]);
+                    }
+                }
+                listArray.Add(list);
+            }
+
+            return listArray;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/118-Pascal's Triangle Test.cs
@@ -0,0 +1,135 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution118Test
+    {
+        [Fact]
+        public void Generate1()
+        {
+            //Arrange 
+            int numRows = 1;
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int>() { 1 }
+            };
+
+            //Act
+            Solution118 s = new Solution118();
+            IList<IList<int>> actual = s.Generate(numRows);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void Generate2()
+        {
+            //Arrange 
+            int numRows = 2;
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int>() { 1 },
+                new List<int>() { 1, 1 }
+            };
+
+            //Act
+            Solution118 s = new Solution118();
+            IList<IList<int>> actual = s.Generate(numRows);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void Generate3()
+        {
+            //Arrange 
+            int numRows = 3;
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int>() { 1 },
+                new List<int>() { 1, 1 },
+                new List<int>() { 1, 2, 1 }
+            };
+
+            //Act
+            Solution118 s = new Solution118();
+            IList<IList<int>> actual = s.Generate(numRows);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+
+        [Fact]
+        public void Generate4()
+        {
+            //Arrange 
+            int numRows = 4;
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int>() { 1 },
+                new List<int>() { 1, 1 },
+                new List<int>() { 1, 2, 1 },
+                new List<int>() { 1, 3, 3, 1 }
+            };
+
+            //Act
+            Solution118 s = new Solution118();
+            IList<IList<int>> actual = s.Generate(numRows);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+
+        [Fact]
+        public void Generate5()
+        {
+            //Arrange 
+            int numRows = 5;
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int>() { 1 },
+                new List<int>() { 1, 1 },
+                new List<int>() { 1, 2, 1 },
+                new List<int>() { 1, 3, 3, 1 },
+                new List<int>() { 1, 4, 6, 4, 1 }
+            };
+
+            //Act
+            Solution118 s = new Solution118();
+            IList<IList<int>> actual = s.Generate(numRows);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+
+        [Fact]
+        public void Generate0()
+        {
+            //Arrange 
+            int numRows = 0;
+            IList<IList<int>> expected = new List<IList<int>>() { };
+
+            //Act
+            Solution118 s = new Solution118();
+            IList<IList<int>> actual = s.Generate(numRows);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
