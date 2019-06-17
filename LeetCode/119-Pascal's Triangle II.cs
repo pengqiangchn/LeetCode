6047c0e733a81d083f2b48d614639ce1f6cf2a9a
彭强 <pengqiangchn@foxmail.com>
2019-06-17 09:26:23 +0800

    119

---
+++ LeetCode/119-Pascal's Triangle II.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution119
+    {
+        //看了解题，可以使用公式方法去计算各行的值。
+        public IList<int> GetRow(int rowIndex)
+        {
+            IList<int> list = new List<int>();
+            list.Add(1);
+            if (rowIndex == 0)
+                return list;
+            list.Add(1);
+            if (rowIndex == 1)
+                return list;
+
+
+            for (int i = 2; i <= rowIndex; i++)
+            {
+                for (int j = 0; j < list.Count - 1; j++)
+                {
+                    list[j] = list[j] + list[j + 1];
+                }
+                list.Insert(0, 1);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/119-Pascal's Triangle II Test.cs
@@ -0,0 +1,92 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution119Test
+    {
+        [Fact]
+        public void GetRow0()
+        {
+            //Arrange
+            int rowIndex = 0;
+            IList<int> expected = new List<int>() { 1 };
+
+            //Act
+            Solution119 s = new Solution119();
+            IList<int> actual = s.GetRow(rowIndex);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void GetRow1()
+        {
+            //Arrange
+            int rowIndex = 1;
+            IList<int> expected = new List<int>() { 1, 1 };
+
+            //Act
+            Solution119 s = new Solution119();
+            IList<int> actual = s.GetRow(rowIndex);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void GetRow2()
+        {
+            //Arrange
+            int rowIndex = 2;
+            IList<int> expected = new List<int>() { 1, 2, 1 };
+
+            //Act
+            Solution119 s = new Solution119();
+            IList<int> actual = s.GetRow(rowIndex);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void GetRow3()
+        {
+            //Arrange
+            int rowIndex = 3;
+            IList<int> expected = new List<int>() { 1, 3, 3, 1 };
+
+            //Act
+            Solution119 s = new Solution119();
+            IList<int> actual = s.GetRow(rowIndex);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+
+        [Fact]
+        public void GetRow4()
+        {
+            //Arrange
+            int rowIndex = 4;
+            IList<int> expected = new List<int>() { 1, 4, 6, 4, 1 };
+
+            //Act
+            Solution119 s = new Solution119();
+            IList<int> actual = s.GetRow(rowIndex);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
