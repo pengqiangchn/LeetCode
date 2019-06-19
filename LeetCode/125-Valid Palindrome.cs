8824bb6c07679805cf40ec0bd0b00db9b8be238b
彭强 <pengqiangchn@foxmail.com>
2019-06-19 13:59:43 +0800

    125

---
+++ LeetCode/125-Valid Palindrome.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution125
+    {
+        //可以不用tolower， 直接进行判断是否为大小写也行
+        public bool IsPalindrome(string s)
+        {
+            s = s.Replace(" ", "").ToLower();
+            int first = 0;
+            int last = s.Length - 1;
+
+            while (first <= last)
+            {
+                if (!(s[first] > 96 && s[first] < 123) && !(s[first] > 47 && s[first] < 58))
+                {
+                    first++;
+                    continue;
+                }
+                if (!(s[last] > 96 && s[last] < 123) && !(s[last] > 47 && s[last] < 58))
+                {
+                    last--;
+                    continue;
+                }
+
+                if (s[first] != s[last])
+                {
+                    return false;
+                }
+                else
+                {
+                    first++;
+                    last--;
+                    continue;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ LeetCodeUnitTest/125-Valid Palindrome Test.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution125Test
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("..")]
+        [InlineData("A man, a plan, a canal: Panama")]
+        public void IsPalindromeTrue(string str)
+        {
+            //Arrange
+            //bool expected = true;
+
+            //Act 
+            Solution125 s = new Solution125();
+            bool actual = s.IsPalindrome(str);
+
+            //Assert
+            Assert.True(actual);
+        }
+
+        [Theory]
+        [InlineData("1121")]
+        [InlineData(" 1231 1231")]
+        [InlineData("race a car")]
+        public void IsPalindromeFalse(string str)
+        {
+            //Arrange
+            //bool expected = true;
+
+            //Act 
+            Solution125 s = new Solution125();
+            bool actual = s.IsPalindrome(str);
+
+            //Assert
+            Assert.False(actual);
+        }
+
+    }
+}
