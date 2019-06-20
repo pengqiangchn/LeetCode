f665faf821185a1c8834ab46fb1b1e1725acc991
彭强 <pengqiangchn@foxmail.com>
2019-06-20 16:59:53 +0800

    136

---
+++ LeetCode/136-Single Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution136
+    {
+        //最快方法 使用位运算
+        public int SingleNumber(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dic.ContainsKey(nums[i]))
+                {
+                    dic.Add(nums[i], 1);
+                }
+                else
+                {
+                    dic[nums[i]] = dic[nums[i]] + 1;
+                }
+            }
+
+            foreach (var key in dic.Keys)
+            {
+                if (dic[key] == 1)
+                    return key;
+            }
+
+            return nums[0];
+        }
+    }
+}
---
+++ LeetCodeUnitTest/136-Single Number Test.cs
@@ -0,0 +1,71 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution136Test
+    {
+        [Fact]
+        public void SingleNumber1()
+        {
+            //Arrange
+            int[] nums = new int[] { 2, 2, 1 };
+            int expected = 1;
+
+            //Act
+            Solution136 s = new Solution136();
+            int actual = s.SingleNumber(nums);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SingleNumber2()
+        {
+            //Arrange
+            int[] nums = new int[] { 4, 1, 2, 1, 2 };
+            int expected = 4;
+
+            //Act
+            Solution136 s = new Solution136();
+            int actual = s.SingleNumber(nums);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SingleNumber3()
+        {
+            //Arrange
+            int[] nums = new int[] { 1 };
+            int expected = 1;
+
+            //Act
+            Solution136 s = new Solution136();
+            int actual = s.SingleNumber(nums);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SingleNumber4()
+        {
+            //Arrange
+            int[] nums = new int[] { 1, 2, 5, 4, 6, 7, 8, 7, 6, 8, 1, 2, 4 };
+            int expected = 5;
+
+            //Act
+            Solution136 s = new Solution136();
+            int actual = s.SingleNumber(nums);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
