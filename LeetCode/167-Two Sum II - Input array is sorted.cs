31967a98bedf551e51c85684ee35d1e7f98f108c
彭强 <pengqiangchn@foxmail.com>
2019-06-22 22:51:35 +0800

    167 使用字典快速写完，这次写了一次都没怎么修改就过了。

---
+++ LeetCode/167-Two Sum II - Input array is sorted.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution167
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            if (numbers.Length < 2)
+                return null;
+
+            //俩个for 暴力解决；
+            //一个for + 字典解决;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (dic.ContainsKey(numbers[i]))
+                {
+                    return new int[] { dic[numbers[i]] + 1, i + 1 };
+                }
+                int sub = target - numbers[i];
+                if (!dic.ContainsKey(sub))
+                {
+                    dic.Add(sub, i);
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/167-Two Sum II - Input array is sorted Test.cs
@@ -0,0 +1,83 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution167Test
+    {
+        [Fact]
+        public void TwoSum1()
+        {
+            //Arrange
+            int[] numbers = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] expected = new int[] { 1, 2 };
+
+            //Act
+            Solution167 s = new Solution167();
+            int[] actual = s.TwoSum(numbers, target);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void TwoSum2()
+        {
+            //Arrange
+            int[] numbers = new int[] { 1, 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] expected = new int[] { 2, 3 };
+
+            //Act
+            Solution167 s = new Solution167();
+            int[] actual = s.TwoSum(numbers, target);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void TwoSum3()
+        {
+            //Arrange
+            int[] numbers = new int[] { 0, 2, 6, 8, 11, 15 };
+            int target = 14;
+
+            int[] expected = new int[] { 3, 4 };
+
+            //Act
+            Solution167 s = new Solution167();
+            int[] actual = s.TwoSum(numbers, target);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void TwoSum4()
+        {
+            //Arrange
+            int[] numbers = new int[] { 0, 2, 6, 8, 9, 11, 15 };
+            int target = 9;
+
+            int[] expected = new int[] { 1, 5 };
+
+            //Act
+            Solution167 s = new Solution167();
+            int[] actual = s.TwoSum(numbers, target);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
