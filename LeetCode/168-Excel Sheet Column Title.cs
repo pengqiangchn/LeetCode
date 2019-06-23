2118129e5b293b9c3283a0e93e9037ba5e7dcdcc
彭强 <pengqiangchn@foxmail.com>
2019-06-23 09:48:58 +0800

    168

---
+++ LeetCode/168-Excel Sheet Column Title.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution168
+    {
+        //看了最快解法，发现可以使用字母'A'作为基准 去加余数。
+        public string ConvertToTitle(int n)
+        {
+            if (n < 1)
+                return "";
+
+            string letters = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string title = "";
+            int index = 0;
+            while (n != 0)
+            {
+                index = n % 26;
+                if (index == 0)
+                {
+                    index = 26;
+                }
+                title = letters[index] + title;
+                n = (n - index) / 26;
+            }
+
+            return title;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/168-Excel Sheet Column Title Test.cs
@@ -0,0 +1,75 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution168Test
+    {
+        [Fact]
+        public void ConvertToTitle1()
+        {
+            //Arrange
+            int n = 1;
+            string expected = "A";
+
+            //Act
+            Solution168 s = new Solution168();
+            string actual = s.ConvertToTitle(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void ConvertToTitle2()
+        {
+            //Arrange
+            int n = 28;
+            string expected = "AB";
+
+            //Act
+            Solution168 s = new Solution168();
+            string actual = s.ConvertToTitle(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void ConvertToTitle3()
+        {
+            //Arrange
+            int n = 701;
+            string expected = "ZY";
+
+            //Act
+            Solution168 s = new Solution168();
+            string actual = s.ConvertToTitle(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void ConvertToTitle4()
+        {
+            //Arrange
+            int n = 9999;
+            string expected = "NTO";
+
+            //Act
+            Solution168 s = new Solution168();
+            string actual = s.ConvertToTitle(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
