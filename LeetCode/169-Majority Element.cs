57009dff93c42deac000d6e91e0ec89cca4f5d1b
彭强 <pengqiangchn@foxmail.com>
2019-06-24 10:57:42 +0800

    169

---
+++ LeetCode/169-Majority Element.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution169
+    {
+        //最快速就是投票法
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    dic[nums[i]]++;
+                }
+                else
+                {
+                    dic.Add(nums[i], 1);
+                }
+            }
+
+            int count = nums.Length / 2;
+            int maxValue = int.MinValue;
+            int maxValueKey = 0;
+            foreach (var item in dic)
+            {
+                if (maxValue < item.Value)
+                {
+                    maxValue = item.Value;
+                    maxValueKey = item.Key;
+                }
+
+                if (item.Value > count)
+                {
+                    return item.Key;
+                }
+            }
+
+            return maxValueKey;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/169-Majority Element Test.cs
@@ -0,0 +1,71 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution169Test
+    {
+        [Fact]
+        public void MajorityElement1()
+        {
+            //Arrange 
+            int[] nums = new int[] { 1, 2, 1 };
+            int expected = 1;
+
+            //Act
+            Solution169 s = new Solution169();
+            int actual = s.MajorityElement(nums);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MajorityElement2()
+        {
+            //Arrange 
+            int[] nums = new int[] { 1, 2, 1, 2, 3, 4, 1, 2, 3, 4, 1, 1 };
+            int expected = 1;
+
+            //Act
+            Solution169 s = new Solution169();
+            int actual = s.MajorityElement(nums);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MajorityElement3()
+        {
+            //Arrange 
+            int[] nums = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+            int expected = 2;
+
+            //Act
+            Solution169 s = new Solution169();
+            int actual = s.MajorityElement(nums);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MajorityElement4()
+        {
+            //Arrange 
+            int[] nums = new int[] { 6, 5, 5 };
+            int expected = 5;
+
+            //Act
+            Solution169 s = new Solution169();
+            int actual = s.MajorityElement(nums);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
