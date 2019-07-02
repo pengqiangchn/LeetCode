dadb4929071af44d6b6ec5b9d52031d6db662f44
彭强 <pengqiangchn@foxmail.com>
2019-07-02 21:20:12 +0800

    231

---
+++ LeetCode/231-Power of Two.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution231
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            //使用位运算
+            return n > 0 && ((n & (n - 1)) == 0);
+
+            //if (n < 0)
+            //    return false;
+
+            //string str = Convert.ToString(n, 2);
+            //int count = 0;
+            //foreach (var item in str)
+            //{
+            //    if (item == '1')
+            //    {
+            //        count++;
+            //    }
+
+            //    if (count > 1)
+            //        break;
+            //}
+
+            //return count == 1;
+        }
+    }
+}
