4456264f721a27b0a6d2ad1f1881028d6ac9e6f9
彭强 <pengqiangchn@foxmail.com>
2019-07-02 21:40:42 +0800

    242

---
+++ LeetCode/242-Valid Anagram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution242
+    {
+        //速度不快， 使用双数组，保存各自的数量，然后再数组比较个数，比较快，只运行了 3个n次；
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            char[] cs = s.ToCharArray();
+            char[] ct = t.ToCharArray();
+            Array.Sort(cs);
+            Array.Sort(ct);
+
+            for (int i = 0; i < cs.Length; i++)
+            {
+                if (cs[i] != ct[i])
+                    return false;
+            }
+
+            return true;
+
+        }
+    }
+}
