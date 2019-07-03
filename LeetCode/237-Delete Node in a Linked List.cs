7cff0c51d73d6cd1562935ddbb479af1c111bad3
彭强 <pengqiangchn@foxmail.com>
2019-07-03 14:24:57 +0800

    237

---
+++ LeetCode/237-Delete Node in a Linked List.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution237
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
