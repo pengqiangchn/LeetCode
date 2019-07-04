21204f7d7a682b9459d8b2fdd62b7e50709ae402
彭强 <pengqiangchn@foxmail.com>
2019-07-04 20:54:46 +0800

    278 需要判断int溢出问题

---
+++ LeetCode/278-First Bad Version.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution278
+    {
+        public int FirstBadVersion(int n)
+        {
+            long preIndex = 1;
+            long endIndex = n;
+
+            if (IsBadVersion(1))
+                return 1;
+
+
+            while (endIndex - preIndex > 1)
+            {
+                long index = (preIndex + endIndex) / 2;
+                if (IsBadVersion((int)index))
+                {
+                    endIndex = index;
+                }
+                else
+                {
+                    preIndex = index;
+                }
+            }
+
+            return (int)endIndex;
+        }
+
+        public bool IsBadVersion(int version)
+        {
+            return version >= 1702766719;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/278-First Bad Version Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution278Test
+    {
+        [Fact]
+        public void FirstBadVersion()
+        {
+            //Arrange 
+            int n = 2126753390;
+            int expected = 1702766719;
+
+            //Act 
+            Solution278 s = new Solution278();
+            int actual = s.FirstBadVersion(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+
+        }
+    }
+}
