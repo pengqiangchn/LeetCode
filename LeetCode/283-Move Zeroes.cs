ebc87cf727b2e03ceb0904145630205422f91516
彭强 <pengqiangchn@foxmail.com>
2019-07-05 14:10:42 +0800

    283

---
+++ LeetCode/283-Move Zeroes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution283
+    {
+        //还有一个简单的方法，就是  覆盖前面元素 ，将0的空缺位置全部覆盖，然后重新赋值后面未处理的数据，均为0
+        public void MoveZeroes(int[] nums)
+        {
+            int index = -1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    if (index == -1)
+                    {
+                        index = i;
+                    }
+                }
+                else
+                {
+                    if (index != -1 && index < nums.Length)
+                    {
+                        nums[index] = nums[i];
+                        nums[i] = 0;
+                        index++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LeetCodeUnitTest/283-Move Zeroes Test.cs
@@ -0,0 +1,76 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution283Test
+    {
+        [Fact]
+        public void MoveZeroes1()
+        {
+            //Arrange
+            int[] nums = new int[] { 0, 1, 0, 3, 12 };
+            int[] expected = new int[] { 1, 3, 12, 0, 0 };
+
+            Solution283 s = new Solution283();
+
+            //Act 
+            s.MoveZeroes(nums);
+
+            //Assert
+            Assert.Equal(expected, nums);
+        }
+
+        [Fact]
+        public void MoveZeroes2()
+        {
+            //Arrange
+            int[] nums = new int[] { 1, 2, 1, 2, 3, 4, 5, 0, 0, 0, 1, 4, 0, 2 };
+            int[] expected = new int[] { 1, 2, 1, 2, 3, 4, 5, 1, 4, 2, 0, 0, 0, 0 };
+
+            Solution283 s = new Solution283();
+
+            //Act 
+            s.MoveZeroes(nums);
+
+            //Assert
+            Assert.Equal(expected, nums);
+        }
+
+        [Fact]
+        public void MoveZeroes3()
+        {
+            //Arrange
+            int[] nums = new int[] { 0, 0, 0, 0, 0, 0 };
+            int[] expected = new int[] { 0, 0, 0, 0, 0, 0 };
+
+            Solution283 s = new Solution283();
+
+            //Act 
+            s.MoveZeroes(nums);
+
+            //Assert
+            Assert.Equal(expected, nums);
+        }
+
+        [Fact]
+        public void MoveZeroes4()
+        {
+            //Arrange
+            int[] nums = new int[] { };
+            int[] expected = new int[] { };
+
+            Solution283 s = new Solution283();
+
+            //Act 
+            s.MoveZeroes(nums);
+
+            //Assert
+            Assert.Equal(expected, nums);
+        }
+
+    }
+}
