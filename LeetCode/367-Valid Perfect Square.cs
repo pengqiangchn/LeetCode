27ac6a8a66f9113617615400f8771bd48e18503f
彭强 <pengqiangchn@foxmail.com>
2019-07-09 19:56:43 +0800

    367

---
+++ LeetCode/367-Valid Perfect Square.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution367
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            if (num <= 0)
+                return false;
+
+            long head = 0;
+            long end = num;
+            while (end - head > 1)
+            {
+                if (num > head * head)
+                {
+                    head = (head + end) / 2;
+                }
+                else if (num < head * head)
+                {
+                    end = head;
+                    head = head / 2;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return num == head * head || num == end * end;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/Easy/367-Valid Perfect Square Test.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution367Test
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(9)]
+        [InlineData(16)]
+        [InlineData(2147395600)]
+        public void IsPerfectSquareTrue(int num)
+        {
+            Solution367 s = new Solution367();
+            bool actual = s.IsPerfectSquare(num);
+
+            Assert.True(actual);
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(8)]
+        [InlineData(12)]
+        public void IsPerfectSquareFalse(int num)
+        {
+            Solution367 s = new Solution367();
+            bool actual = s.IsPerfectSquare(num);
+
+            Assert.False(actual);
+
+        }
+    }
+}
