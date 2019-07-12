b4c20937260efa99fb28381afc36c01bd6c381d2
彭强 <pengqiangchn@foxmail.com>
2019-07-12 21:55:13 +0800

    383,387

---
+++ LeetCode/383-Ransom Note.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution383
+    {
+        //前面写的误解题目了。
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            int[] chars = new int[26];
+
+            for (int i = 0; i < magazine.Length; i++)
+            {
+                chars[magazine[i] - 'a']++;
+            }
+
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                chars[ransomNote[i] - 'a']--;
+                if (chars[ransomNote[i] - 'a'] < 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+
+
+            //if (string.IsNullOrWhiteSpace(ransomNote))
+            //    return true;
+
+            //if (magazine.Length < ransomNote.Length)
+            //    return false;
+
+            //for (int i = 0; i < magazine.Length; i++)
+            //{
+            //    if (magazine[i] == ransomNote[0] && i + ransomNote.Length - 1 < magazine.Length)
+            //    {
+            //        for (int j = 1; j < ransomNote.Length; j++)
+            //        {
+            //            if (magazine[i + j] != ransomNote[j])
+            //            {
+            //                break;
+            //            }
+
+            //            if (j == ransomNote.Length - 1)
+            //            {
+            //                return true;
+            //            }
+            //        }
+            //    }
+            //}
+
+            //return false;
+        }
+    }
+}
---
+++ LeetCode/387-First Unique Character in a String.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution387
+    {
+        public int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            Dictionary<char, int> index = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dic.ContainsKey(s[i]))
+                {
+                    dic[s[i]]++;
+                }
+                else
+                {
+                    dic[s[i]] = 1;
+                }
+
+                if (!index.ContainsKey(s[i]))
+                {
+                    index[s[i]] = i;
+                }
+            }
+
+            foreach (var key in dic.Keys)
+            {
+                if (dic[key] == 1)
+                {
+                    return index[key];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
