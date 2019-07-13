41ac19e6321b145b3e3ebdcb93262fa3825e0af2
彭强 <pengqiangchn@foxmail.com>
2019-07-13 11:32:20 +0800

    389,409

---
+++ LeetCode/389-Find the Difference.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution389
+    {
+        //还可以 直接使用 求和，因为只有字母，那么求和以后 t的和-s的和就是答案
+        public char FindTheDifference(string s, string t)
+        {
+            int c = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                c ^= s[i];
+
+                c ^= t[i];
+            }
+
+            c ^= t[t.Length - 1];
+
+            return (char)c;
+
+
+            //int i = 0;
+            //foreach (var c in s)
+            //{
+            //    i ^= c;
+            //}
+
+            //foreach (var c in t)
+            //{
+            //    i ^= c;
+            //}
+
+            //return (char)i;
+        }
+    }
+}
---
+++ LeetCode/409-Longest Palindrome.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution409
+    {
+        //这里只需要计数就行，用数组也是可以的。
+        public int LongestPalindrome(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return 0;
+
+            //查找所有字符，整理出现次数
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            foreach (var c in s)
+            {
+                if (dic.ContainsKey(c))
+                {
+                    dic[c]++;
+                }
+                else
+                {
+                    dic[c] = 1;
+                }
+            }
+
+            //记录所有双数的字符
+            int count = 0;
+            foreach (var key in dic.Keys)
+            {
+                if (dic[key] / 2 > 0)
+                {
+                    count += dic[key] - dic[key] % 2;
+                }
+            }
+
+            //如果未全部使用，可以再那一个单数作为中间轴；
+            if (count < s.Length)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
