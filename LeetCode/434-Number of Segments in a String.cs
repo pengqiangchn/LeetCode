9b415a7c5de7eb95d2dee95d2361283b58e3bb14
彭强 <pengqiangchn@foxmail.com>
2019-07-15 21:58:05 +0800

    434

---
+++ LeetCode/434-Number of Segments in a String.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution434
+    {
+        public int CountSegments(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return 0;
+            s = s.Trim();
+
+            int count = 0;
+            bool perChar = false;
+
+            foreach (var c in s)
+            {
+                if (c != ' ')
+                {
+                    if (!perChar)
+                    {
+                        perChar = true;
+                        count++;
+                    }
+                }
+                else
+                {
+                    perChar = false;
+                }
+            }
+
+            return count;
+        }
+    }
+}
