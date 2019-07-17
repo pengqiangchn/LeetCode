9d621e6d31a0e26b038f2f01d009498f1f21cc76
彭强 <pengqiangchn@foxmail.com>
2019-07-17 21:20:59 +0800

    441

---
+++ LeetCode/441-Arranging Coins.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution441
+    {
+        public int ArrangeCoins(int n)
+        {
+            int i = (int)Math.Sqrt(2.0 * n);
+
+            while (i <= n)
+            {
+                if ((long)(1 + i) * i / 2 == n)
+                {
+                    return i;
+                }
+                else if ((long)(1 + i) * i / 2 > n)
+                {
+                    return i - 1;
+                }
+                i++;
+            }
+
+            return i;
+        }
+    }
+}
---
+++ LeetCodeUnitTest/441-Arranging Coins Test.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution441Test
+    {
+        [Fact]
+        public void ArrangeCoins()
+        {
+            int n = 2147483647;
+            int expected = 65535;
+
+            Solution441 s = new Solution441();
+
+            int actual = s.ArrangeCoins(n);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
