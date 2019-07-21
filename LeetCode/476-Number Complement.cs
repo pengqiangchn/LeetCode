7b0657d82f679c6e5bdcb4a6512bd937a7448b6e
彭强 <pengqiangchn@foxmail.com>
2019-07-21 19:43:37 +0800

    476

---
+++ LeetCode/476-Number Complement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Sloution476
+    {
+        public int FindComplement(int num)
+        {
+            string str = Convert.ToString(num, 2).TrimStart('0');
+            string com = "";
+            foreach (var s in str)
+            {
+                if (s == '1')
+                {
+                    com += "0";
+                }
+                else
+                {
+                    com += "1";
+                }
+            }
+
+            return Convert.ToInt32(com, 2);
+        }
+    }
+}
