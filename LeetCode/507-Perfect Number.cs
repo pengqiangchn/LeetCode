a81ba8035c98ca3d1ef409c1c635a505d3f42a85
彭强 <pengqiangchn@foxmail.com>
2019-07-23 21:33:31 +0800

    507 509

---
+++ LeetCode/507-Perfect Number.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution507
+    {
+        public bool CheckPerfectNumber(int num)
+        {
+            //int sum = 0;
+            //for (int i = num / 2; i > 0; i--)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        sum += i;
+            //        if (sum > num)
+            //        {
+            //            return false;
+            //        }
+            //        else if (sum == num)
+            //        {
+            //            return true;
+            //        }
+            //    }
+            //}
+
+            //return false;
+            if (num == 1)
+                return false;
+
+            int sum = 1; int i = 2;
+            for (; i < Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                    sum += (num / i);
+                }
+
+            }
+
+            if (i * i == num)
+            {
+                sum += i;
+            }
+
+            return sum == num;
+
+        }
+    }
+}
---
+++ LeetCode/509-Fibonacci Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution509
+    {
+        public int Fib(int N)
+        {
+            int f1 = 1, f2 = 1, f3 = 0;
+            if (N == 0)
+                return 0;
+            if (N <= 2)
+            {
+                return 1;
+            }
+
+            for (int i = 0; i < N - 2; i++)
+            {
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+            }
+
+            return f3;
+        }
+    }
+}
