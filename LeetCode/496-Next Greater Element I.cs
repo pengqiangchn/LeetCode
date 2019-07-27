05ce8ddaa22f51220e85633c3afd7d777284732d
彭强 <pengqiangchn@foxmail.com>
2019-07-27 21:22:30 +0800

    496

---
+++ LeetCode/496-Next Greater Element I.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution469
+    {
+        public int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            if (nums1.Length == 0)
+                return nums1;
+
+            List<int> list = new List<int>();
+
+            Dictionary<int, int> dic = new Dictionary<int, int>() { };
+
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                int j = i + 1;
+                for (; j < nums2.Length; j++)
+                {
+                    if (nums2[i] < nums2[j])
+                    {
+                        dic[nums2[i]] = nums2[j];
+                        break;
+                    }
+                }
+                if (j == nums2.Length)
+                {
+                    dic[nums2[i]] = -1;
+                }
+            }
+
+            foreach (var num in nums1)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    list.Add(dic[num]);
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ LeetCodeUnitTest/496-Next Greater Element I Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTest
+{
+    public class Solution496Test
+    {
+        [Fact]
+        public void NextGreaterElement()
+        {
+            int[] nums1 = new int[] { 4, 1, 2 };
+            int[] nums2 = new int[] { 1, 3, 4, 2 };
+
+            int[] expected = new int[] { -1, 3, -1 };
+
+            Solution469 s = new Solution469();
+
+            int[] actual = s.NextGreaterElement(nums1, nums2);
+
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
