65db88b5df9226f065ee48a1d3ea5197a29975e8
彭强 <pengqiangchn@foxmail.com>
2019-08-04 11:20:21 +0800

    728,771

---
+++ LeetCode/728-Self Dividing Numbers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution728
+    {
+        public IList<int> SelfDividingNumbers(int left, int right)
+        {
+            IList<int> list = new List<int>();
+
+            for (int i = left; i <= right; i++)
+            {
+                if (i % 10 == 0)
+                    continue;
+
+                if (i < 10)
+                {
+                    list.Add(i);
+                }
+                else
+                {
+                    int num = i;
+                    while (num != 0)
+                    {
+                        int temp = num % 10;
+                        if (temp == 0)
+                        {
+                            break;
+                        }
+
+                        if (i % temp == 0)
+                        {
+                            num = num / 10;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (num == 0)
+                    {
+                        list.Add(i);
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ LeetCode/771-Jewels and Stones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution771
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            int count = 0;
+
+            for (int i = 0; i < J.Length; i++)
+            {
+                for (int j = 0; j < S.Length; j++)
+                {
+                    if (J[i] == S[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+
+            return count;
+
+        }
+    }
+}
