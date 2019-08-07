a0fbdbc8ef0042bbe51286368ee871fe711cac0a
彭强 <pengqiangchn@foxmail.com>
2019-08-07 20:43:53 +0800

    724 804

---
+++ LeetCode/724-Find Pivot Index.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution724
+    {
+        public int PivotIndex(int[] nums)
+        {
+            if (nums.Length < 3)
+                return -1;
+
+            int right = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                right += nums[i];
+            }
+
+            if (right == 0)
+                return 0;
+
+            int left = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                right -= nums[i];
+                if (left == right)
+                    return i;
+                left += nums[i];
+            }
+
+            return -1;
+
+        }
+    }
+}
---
+++ LeetCode/804-Unique Morse Code Words.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution804
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            string[] str = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+            HashSet<string> hash = new HashSet<string>();
+
+            foreach (var word in words)
+            {
+                string s = "";
+                foreach (var w in word)
+                {
+                    s += (str[w - 'a']);
+                }
+                hash.Add(s);
+            }
+
+            return hash.Count;
+        }
+    }
+}
