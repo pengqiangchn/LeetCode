7b2117db7ecb4ead44a3516f0c10ddf0e7d7007e
彭强 <pengqiangchn@foxmail.com>
2019-08-08 20:43:50 +0800

    905

---
+++ LeetCode/905-Sort Array By Parity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution905
+    {
+        public int[] SortArrayByParity(int[] A)
+        {
+            if (A.Length < 2)
+                return A;
+
+            int right = A.Length - 1;
+            int left = 0;
+            int temp = 0;
+            while (left < right)
+            {
+                if (A[left] % 2 == 1)
+                {
+                    //找到从右到左偶数
+                    while (left < right && A[right] % 2 == 1)
+                    {
+                        right--;
+                    }
+                    temp = A[left];
+                    A[left] = A[right];
+                    A[right] = temp;
+                }
+                left++;
+            }
+
+            return A;
+        }
+    }
+}
