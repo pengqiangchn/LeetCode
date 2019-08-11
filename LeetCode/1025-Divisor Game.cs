2cc14d333e5da7c56672262ab8bb9725f70d529c
彭强 <pengqiangchn@foxmail.com>
2019-08-11 22:00:48 +0800

    1025

---
+++ LeetCode/1025-Divisor Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution1025
+    {
+        public bool DivisorGame(int N)
+        {
+            return N % 2 == 0;
+        }
+    }
+}
