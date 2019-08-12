b3be11101db072df4fd025e666bac3f74aa426d1
彭强 <pengqiangchn@foxmail.com>
2019-08-12 20:26:25 +0800

    557

---
+++ LeetCode/557-Reverse Words in a String III.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution557
+    {
+        public string ReverseWords(string s)
+        {
+            string ss = "";
+            foreach (var str in s.Split(" "))
+            {
+                char[] cs = str.ToCharArray();
+
+                Array.Reverse(cs);
+
+                ss += new string(cs) + " ";
+            }
+
+            return ss.Trim();
+        }
+    }
+}
