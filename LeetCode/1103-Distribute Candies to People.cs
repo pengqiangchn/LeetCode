88f5eaf3f101c5d6ccae8784ba89080d3583e187
彭强 <pengqiangchn@foxmail.com>
2019-08-30 23:05:35 +0800

    1103

---
+++ LeetCode/1103-Distribute Candies to People.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution1103
+    {
+        public int[] DistributeCandies(int candies, int num_people)
+        {
+            int[] p = new int[num_people];
+            int count = 1;
+            int index = 0;
+            while (candies > 0)
+            {
+                if (candies > count)
+                {
+
+                    p[index % num_people] += count;
+                    candies -= count;
+                    count++;
+                }
+                else
+                {
+                    p[index % num_people] += candies;
+                    candies = 0;
+                }
+                index++;
+            }
+
+
+            return p;
+        }
+    }
+}
