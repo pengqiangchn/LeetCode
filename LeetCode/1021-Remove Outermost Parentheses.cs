51a1858e05751d3d273c2aa2ecb95d767bd83da3
彭强 <pengqiangchn@foxmail.com>
2019-09-02 22:56:30 +0800

    1021,1071

---
+++ LeetCode/1021-Remove Outermost Parentheses.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution1021
+    {
+        public string RemoveOuterParentheses(string S)
+        {
+            string str = "";
+            int count = 0;
+
+            string ss = "";
+            foreach (var c in S)
+            {
+                if (c == '(')
+                {
+                    if (count != 0)
+                    {
+                        ss += "(";
+                    }
+                    count++;
+                }
+                else
+                {
+                    count--;
+                    if (count != 0)
+                    {
+                        ss += ")";
+                    }
+                }
+
+                if (count == 0)
+                {
+                    str += ss;
+                    ss = "";
+                }
+            }
+
+
+            return str;
+        }
+    }
+}
