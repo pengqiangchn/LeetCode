a5b7bcfce413d029143820684004fc93dbdd52e1
彭强 <pengqiangchn@foxmail.com>
2019-09-14 21:26:45 +0800

    1185

---
+++ LeetCode/1185-Day of the Week.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution1185
+    {
+        public string DayOfTheWeek(int day, int month, int year)
+        {
+            DateTime dt = new DateTime(year, month, day);
+
+            return dt.DayOfWeek.ToString();
+        }
+    }
+}
