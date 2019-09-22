32ed6a7168814a60e16bf677237c43e7c680bc1b
彭强 <pengqiangchn@foxmail.com>
2019-09-22 20:42:38 +0800

    1184

---
+++ LeetCode/1184-Distance Between Bus Stops.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution1184
+    {
+        public int DistanceBetweenBusStops(int[] distance, int start, int destination)
+        {
+            if (start == destination)
+            {
+                return 0;
+            }
+
+            int stops = 0;
+
+            if (start < destination)
+            {
+                for (int i = start; i < destination; i++)
+                {
+                    stops += distance[i];
+                }
+            }
+            else
+            {
+                for (int i = destination; i < start; i++)
+                {
+                    stops += distance[i];
+                }
+
+            }
+
+            return Math.Min(distance.Sum() - stops, stops);
+        }
+    }
+}
